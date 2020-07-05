494ad46f3359b9941c5aba00c1fac22d20881917
ashmithatalluri <67856031+ashmithatalluri@users.noreply.github.com>
2020-07-05 13:39:17 +0530

    Created
    These files shows
    1. Area of the circle
    2.Program to accept a filename from the user and prints the extension of that.

---
+++ proj1.py
@@ -0,0 +1,8 @@
+a=float(input("Input the radius of the circle:"))
+Pi=3.14159
+area= Pi * pow(a,2)
+print("The area of the circle with radius " +str(a) + " is: " +str(area))
+
+
+      
+#AREA OF THE CIRCLE
---
+++ proj2.py
@@ -0,0 +1,4 @@
+a=input("Input the filename:")
+ext = a.split(".")         #SPLITS THE SPACES OR SPLITS AT ','.
+print("The extension of the file is: " + repr(ext[-1]))     #RETURNS A STRING THAT HOLDS A PRINTABLE REPRESENTATION OF AN OBJECT.
+                                                             
